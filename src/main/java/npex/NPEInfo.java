0a61c425a2bbdf16c374cdae3ca9dc5d769d079e
SeongjoonHong <seongjoon@korea.ac.kr>
2020-10-06 12:36:26 +0900

    [Fix] NPE resolver에서 expression 샅샅이 뒤지도록 수정

--- src/main/java/npex/NPEInfo.java
+++ src/main/java/npex/NPEInfo.java
@@ -5,7 +5,9 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import org.json.JSONObject;
 
@@ -16,37 +18,38 @@ import spoon.reflect.code.CtLiteral;
 import spoon.reflect.code.CtTargetedExpression;
 import spoon.reflect.code.CtVariableRead;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.factory.Factory;
+import spoon.reflect.visitor.CtScanner;
 import spoon.reflect.visitor.filter.NamedElementFilter;
-import spoon.reflect.visitor.filter.TypeFilter;
 
 public class NPEInfo {
   String filepath;
   int line;
   String deref_field;
-  CtMethod<?> sink_method;
-  CtClass<?> sink_class;
+  CtMethod<?> npe_method;
+  CtClass<?> npe_class;
+  CtInvocation<?> callee;
 
   public static NPEInfo readFromJSON(Factory factory, String jsonPath) throws IOException {
-    String contents = new String(Files.readAllBytes(Paths.get(jsonPath)));
-    JSONObject js = new JSONObject(contents);
+    JSONObject js = new JSONObject(new String(Files.readAllBytes(Paths.get(jsonPath))));
     String filepath = js.getString("filepath");
     int line = js.getInt("line");
     String deref_field = js.getString("deref_field");
-    String sink_class_name = js.getString("npe_class");
-    CtClass<?> sink_class = factory.getModel().getElements(new NamedElementFilter<>(CtClass.class, sink_class_name))
+    String npe_class_name = js.getString("npe_class");
+    CtClass<?> npe_class = factory.getModel().getElements(new NamedElementFilter<>(CtClass.class, npe_class_name))
         .get(0);
-    CtMethod<?> sink_method = null;
-    return new NPEInfo(filepath, line, deref_field, sink_method, sink_class);
+    CtMethod<?> npe_method = null;
+    return new NPEInfo(filepath, line, deref_field, npe_method, npe_class);
   }
 
-  public NPEInfo(String filepath, int line, String deref_field, CtMethod<?> sink_method, CtClass<?> sink_class) {
+  public NPEInfo(String filepath, int line, String deref_field, CtMethod<?> npe_method, CtClass<?> npe_class) {
     this.filepath = filepath;
     this.line = line;
     this.deref_field = deref_field;
-    this.sink_method = sink_method;
-    this.sink_class = sink_class;
+    this.npe_method = npe_method;
+    this.npe_class = npe_class;
   }
 
   public void writeToJSON(File outFile) throws IOException {
@@ -54,8 +57,8 @@ public class NPEInfo {
     js.put("filepath", filepath);
     js.put("line", line);
     js.put("deref_field", deref_field);
-    js.put("sink_method", sink_method.getSimpleName());
-    js.put("npe_class", sink_class.getSimpleName());
+    js.put("npe_method", npe_method.getSimpleName());
+    js.put("npe_class", npe_class.getSimpleName());
 
     FileWriter writer = new FileWriter(outFile);
     writer.write(js.toString(4));
@@ -63,10 +66,10 @@ public class NPEInfo {
   }
 
   public CtExpression<?> resolve() throws NoSuchElementException {
-    for (CtExpression<?> expr : sink_class.getElements(new TypeFilter<>(CtExpression.class))) {
-      if (!expr.getPosition().isValidPosition() || expr.getPosition().getLine() != line)
-        continue;
+    NPEScanner scanner = new NPEScanner();
+    npe_class.accept(scanner);
 
+    for (CtExpression expr : scanner.getExpressions()) {
       if (expr instanceof CtVariableRead
           && deref_field.equals(((CtVariableRead<?>) expr).getVariable().getSimpleName()))
         return expr;
@@ -83,7 +86,7 @@ public class NPEInfo {
       }
 
       if (expr instanceof CtLiteral) {
-        if (((CtLiteral<?>) expr).toString().equals("null"))
+        if (deref_field.equals("null") && ((CtLiteral<?>) expr).toString().equals("null"))
           return expr;
         continue;
       }
@@ -106,5 +109,26 @@ public class NPEInfo {
     }
 
     throw new IllegalArgumentException();
+  }
+
+  private class NPEScanner extends CtScanner {
+    private Set<CtExpression> expressions = new HashSet<>();
+
+    private boolean isLineMatched(CtElement e) {
+      return e.getPosition().isValidPosition() && e.getPosition().getLine() == line;
+    }
+
+    public Set<CtExpression> getExpressions() {
+      return expressions;
+    }
+
+    @Override
+    public void scan(CtElement e) {
+      super.scan(e);
+      if (e instanceof CtExpression && isLineMatched(e)) {
+        expressions.add((CtExpression) e);
+      }
+    }
+
   }
 }
