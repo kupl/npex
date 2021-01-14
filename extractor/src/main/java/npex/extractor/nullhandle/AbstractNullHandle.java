326523388e1bb8c3dcd783c56aecf15f72b5d6ca
SeongjoonHong <seongjoon@korea.ac.kr>
2021-01-14 18:33:03 +0900

    add extractor module & define nullhandle intf.

---
+++ extractor/src/main/java/npex/extractor/nullhandle/AbstractNullHandle.java
@@ -0,0 +1,53 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2020 Software Analysis Laboratory, Korea University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package npex.extractor.nullhandle;
+
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.declaration.CtElement;
+import spoon.reflect.factory.CoreFactory;
+import spoon.support.DefaultCoreFactory;
+
+public abstract class AbstractNullHandle<T extends CtElement> {
+  final protected T handle;
+  final protected CtBinaryOperator nullCond;
+  final protected CtExpression nullExpr;
+  final protected CoreFactory factory = new DefaultCoreFactory();
+
+  public AbstractNullHandle(T handle, CtBinaryOperator nullCond) {
+    this.handle = handle;
+    this.nullCond = nullCond;
+    this.nullExpr = nullCond.getLeftHandOperand();
+  }
+
+  public T getHandle() {
+    return handle;
+  }
+
+  public CtExpression getNullExpr() {
+    return nullExpr;
+  }
+
+  public abstract CtExpression getModelExpr();
+}
