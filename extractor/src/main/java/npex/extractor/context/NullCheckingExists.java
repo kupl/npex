4d56b515acbf3eab27985784c05e4649f78343d3
SeongjoonHong <seongjoon@korea.ac.kr>
2021-02-15 13:29:43 +0900

    add contexts info to null model

---
+++ extractor/src/main/java/npex/extractor/context/NullCheckingExists.java
@@ -0,0 +1,60 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2020 Software Analysis Laboratory, Korea University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package npex.extractor.context;
+
+import npex.common.filters.MethodOrConstructorFilter;
+import npex.common.utils.ASTUtils;
+import spoon.reflect.code.CtBinaryOperator;
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.visitor.EarlyTerminatingScanner;
+
+public class NullCheckingExists implements Context {
+  public Boolean extract(CtInvocation invo, int nullPos) {
+    CtExecutable exec = invo.getParent(new MethodOrConstructorFilter());
+    CtExpression nullExp = nullPos == -1 ? invo.getTarget() : (CtExpression) invo.getArguments().get(nullPos);
+    ContextScanner scanner = new ContextScanner(invo, nullExp);
+    exec.accept(scanner);
+    return scanner.getResult();
+  }
+
+  private class ContextScanner extends EarlyTerminatingScanner<Boolean> {
+    final private CtExpression nullExp;
+
+    public ContextScanner(CtInvocation invo, CtExpression nullExp) {
+      this.nullExp = nullExp;
+      setResult(false);
+    }
+
+    public void visitCtBinaryOperator(CtBinaryOperator cond) {
+      super.visitCtBinaryOperator(cond);
+      if (ASTUtils.isNullCondition(cond) && cond.getLeftHandOperand().equals(nullExp)) {
+        setResult(true);
+        terminate();
+      }
+    }
+  }
+
+}
---
+++ extractor/src/main/java/npex/extractor/context/UsedAsArgument.java
@@ -0,0 +1,48 @@
+/**
+ * The MIT License
+ *
+ * Copyright (c) 2020 Software Analysis Laboratory, Korea University
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package npex.extractor.context;
+
+import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtInvocation;
+import spoon.reflect.declaration.ParentNotInitializedException;
+import spoon.reflect.path.CtRole;
+import spoon.reflect.visitor.filter.AbstractFilter;
+
+public class UsedAsArgument implements Context {
+  public Boolean extract(CtInvocation invo, int nullPos) {
+    try {
+      invo.getParent(new ArgumentFilter());
+      return true;
+    } catch (ParentNotInitializedException e) {
+      return false;
+    }
+
+  }
+
+  private class ArgumentFilter extends AbstractFilter<CtExpression> {
+    public boolean matches(CtExpression e) {
+      return e.getRoleInParent().equals(CtRole.ARGUMENT);
+    }
+  }
+}
