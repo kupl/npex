b8958b3f99e778b4d1dbf366643efbb271c81ce8
SeongjoonHong <seongjoon@korea.ac.kr>
2021-04-22 10:59:36 +0900

    Fix NPE in context features

--- extractor/src/main/java/npex/extractor/context/LHSIsPrivate.java
+++ extractor/src/main/java/npex/extractor/context/LHSIsPrivate.java
@@ -7,7 +7,7 @@ import spoon.reflect.code.CtVariableWrite;
 public class LHSIsPrivate implements Context {
   public Boolean extract(CtInvocation invo, int nullPos) {
     if (invo.getParent(CtAssignment.class) instanceof CtAssignment assignment) {
-      return assignment.getAssigned() instanceof CtVariableWrite write
+      return assignment.getAssigned() instanceof CtVariableWrite write && write.getVariable().getDeclaration() != null
           && write.getVariable().getDeclaration().isPrivate();
     } else {
       return false;
