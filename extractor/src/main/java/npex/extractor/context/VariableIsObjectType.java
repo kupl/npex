6ddbb46b30e2acbb600a7e06845bd97f0e0cad80
SeongjoonHong <seongjoon@korea.ac.kr>
2021-05-18 18:15:06 +0900

    [Fix] getType() in VariableIsObjectType can return null

--- extractor/src/main/java/npex/extractor/context/VariableIsObjectType.java
+++ extractor/src/main/java/npex/extractor/context/VariableIsObjectType.java
@@ -5,6 +5,6 @@ import spoon.reflect.code.CtVariableAccess;
 public class VariableIsObjectType extends AbstractVariableContext {
   @Override
   protected Boolean extract(CtVariableAccess va) {
-    return va.getType().getQualifiedName().equals("java.lang.Object");
+    return va.getType() != null && va.getType().getQualifiedName().equals("java.lang.Object");
   }
 }
